62aa12d29a3c8d749a3cede8b40931a533fa92f5
yjp19871013 <yjp19871013@126.com>
2020-03-16 18:27:31 +0800

    http client 添加delete

--- http_client/http_client.go
+++ http_client/http_client.go
@@ -1,6 +1,7 @@
 package http_client
 
 import (
+	"com.fs/event-service/utils"
 	"net/http"
 	"strings"
 	"time"
@@ -35,6 +36,19 @@ func DestroyHttpClient(client *HTTPClient) {
 func (client *HTTPClient) Post(url string, request string, contentType string) (*http.Response, error) {
 	req, err := http.NewRequest("POST", url, strings.NewReader(request))
 	if err != nil {
+		utils.PrintCallErr("HTTPClient.Post", "http.NewRequest", err)
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", contentType)
+
+	return client.httpClient.Do(req)
+}
+
+func (client *HTTPClient) Delete(url string, contentType string) (*http.Response, error) {
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		utils.PrintCallErr("HTTPClient.Delete", "http.NewRequest", err)
 		return nil, err
 	}
 
